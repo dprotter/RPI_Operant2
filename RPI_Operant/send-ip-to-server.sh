ffede7bd7703949111ff47dcfdaa1bd1e3d74c28
dprotter <david.protter@gmail.com>
2022-04-26 16:03:40 -0600

    add

---
+++ RPI_Operant/send-ip-to-server.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+# send-ip-to-server
+
+# VARIABLES
+USERNAME=pi
+SERVER_NAME="mcdb70.colorado.edu"
+PI_PATH="/home/pi/uploads/"
+
+sleep 30
+
+ip -4 -br addr | grep UP | awk '{print $3}' | awk -F"/" '{print $1}' > IP-$( ho$
+echo "$( date )" >> IP-$( hostname -s ).txt
+scp IP-$( hostname -s ).txt "${USERNAME}@${SERVER_NAME}":"${PI_PATH}".
+rm IP-$( hostname -s ).txt
+
+exit 0
