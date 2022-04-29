b7ac01704a5864a9bf4d726d1ed83277637c137c
david protter <david.protter@gmail.com>
2022-04-29 15:12:17 -0600

    local bug

--- RPI_Operant/send-ip-to-server.sh
+++ RPI_Operant/send-ip-to-server.sh
@@ -1,5 +1,5 @@
-
 #!/bin/bash
+
 # send-ip-to-server
 
 # VARIABLES
@@ -9,7 +9,7 @@ PI_PATH="/home/pi/uploads/"
 
 sleep 30
 
-ip -4 -br addr | grep UP | awk '{print $3}' | awk -F"/" '{print $1}' > IP-$( ho$
+ip -4 -br addr | grep UP | awk '{print $3}' | awk -F"/" '{print $1}' > IP-$( hostname -s ).txt
 echo "$( date )" >> IP-$( hostname -s ).txt
 scp IP-$( hostname -s ).txt "${USERNAME}@${SERVER_NAME}":"${PI_PATH}".
 rm IP-$( hostname -s ).txt
