329ee65384d7fd4c84ee83323eaf79e1defd4d49
David Protter <david.protter@gmail.com>
2022-06-28 12:07:40 -0600

    tweaks

--- RPI_Operant/default_scripts/contingent_2_lever_test.py
+++ RPI_Operant/default_scripts/contingent_2_lever_test.py
@@ -34,9 +34,7 @@ def run():
         pellets_dispenser_2_remaining = dispenser_2.config_dict['max_pellets']
         for i in range(1,box.software_config['values']['rounds']+1, 1):
             
-            #testing
-            press = random.randint(0,2)
-            lat = 1
+
             
             box.timing.new_round(length = box.software_config['values']['round_length'])
             lever_phase = box.timing.new_phase(f'lever_out', length = box.software_config['values']['lever_out_time'])
@@ -48,15 +46,8 @@ def run():
             press_latency_2 = lever_2.extend()
             lever_2.wait_for_n_presses(n=1, latency_obj = press_latency_2)
             
-            #testing
-            start = time.time()
+
             while lever_phase.active():
-                
-                if press in [1,2] and time.time() - start > lat:
-                    if press ==1:
-                        lever_1.simulate_pressed()
-                    else:
-                        lever_2.simulate_pressed()
                 
                 if lever_1.presses_reached:
                     
