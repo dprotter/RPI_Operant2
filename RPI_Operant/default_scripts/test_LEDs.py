4f05f8f91d763c47df2e226dd16fe473f0c9fceb
dprotter <david.protter@gmail.com>
2022-09-07 15:36:35 -0600

    forgot to add new script

---
+++ RPI_Operant/default_scripts/test_LEDs.py
@@ -0,0 +1,26 @@
+from RPI_Operant.hardware.box import Box
+import time
+import random
+from tabulate import tabulate
+import RPi.GPIO as GPIO
+
+import argparse
+import os
+
+RUNTIME_DICT = {'vole':000, 'day':1, 'experiment':'cooperant_magazine', 'side':1}
+USER_HARDWARE_CONFIG_PATH = '/home/pi/RPI_Operant2/RPI_Operant/default_setup_files/default_hardware.yaml'
+USER_SOFTWARE_CONFIG_PATH = '/home/pi/RPI_Operant2/RPI_Operant/default_setup_files/cooperant_magazine.yaml'
+
+box = Box()
+box.setup(run_dict=RUNTIME_DICT, 
+            user_hardware_config_file_path=USER_HARDWARE_CONFIG_PATH,
+            user_software_config_file_path=USER_SOFTWARE_CONFIG_PATH,
+            start_now=True)
+
+for output in box.outputs:
+    for _ in range(4):
+        output.activate()
+        time.sleep(0.25)
+        output.deactivate()
+
+box.shutdown()
