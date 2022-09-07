9138cfc0e86f64ae8d6b896bdcf0cc53b2896fd5
Ryan Cameron <ryca1116@colorado.edu>
2022-09-07 11:22:33 -0600

    Fixed calibration

--- RPI_Operant/default_scripts/calibrate_levers.py
+++ RPI_Operant/default_scripts/calibrate_levers.py
@@ -3,8 +3,8 @@ from RPI_Operant.hardware.box import Box
 import time
 import random
 
-USER_CONFIG_PATH = '/home/pi/RPI_Operant2/RPI_Operant/default_setup_files/default_cooperant_hardware.yaml'
-USER_SOFTWARE_CONFIG_PATH = '/home/pi/RPI_Operant2/RPI_Operant/default_setup_files/cooperant_magazine.yaml'
+USER_CONFIG_PATH = '/home/pi/RPI_Operant2/RPI_Operant/default_setup_files/default_hardware.yaml'
+USER_SOFTWARE_CONFIG_PATH = '/home/pi/RPI_Operant2/RPI_Operant/default_setup_files/default_software.yaml'
 
 box = Box()    
 box.setup(user_hardware_config_file_path=USER_CONFIG_PATH,
@@ -18,6 +18,17 @@ def calibrate_lever(lever):
     inp =input(f'press enter to extend lever:{l.name} or "s" to skip\n')
     if not inp == 's':
         print(f'lever extended val is {l.config_dict["extended"]}. try a new value. enter "s" to exit.\n')
+        while not inp == 's':
+            inp = input()
+            if not inp == 's':
+                try:
+                    l.servo.angle = int(inp)
+                except:
+                    print('out of range')
+
+    inp =input(f'press enter to retract lever:{l.name} or "s" to skip\n')
+    if not inp == 's':
+        print(f'lever retracted val is {l.config_dict["retracted"]}. try a new value. enter "s" to exit.\n')
         while not inp == 's':
             inp = input()
             if not inp == 's':
