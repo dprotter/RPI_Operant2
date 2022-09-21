d34adb101fe61bbe1292dd8b348898e080c7cc73
dprotter <david.protter@gmail.com>
2022-09-21 15:22:34 -0600

    testing_triggers

--- RPI_Operant/default_scripts/test_LEDs.py
+++ RPI_Operant/default_scripts/test_LEDs.py
@@ -15,7 +15,11 @@ box = Box()
 box.setup(run_dict=RUNTIME_DICT, 
             user_hardware_config_file_path=USER_HARDWARE_CONFIG_PATH,
             user_software_config_file_path=USER_SOFTWARE_CONFIG_PATH,
-            start_now=True)
+            start_now=False)
+
+time.sleep(0.5)
+trigger_obj = box.outputs.miniscope_trigger
+box.start_and_trigger(trigger_obj)
 
 for output in box.outputs:
     print(f'testing output: {output.name}')
