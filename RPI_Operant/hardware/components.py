447866fb56cd687b13db7c32ecd067dc233aa6ce
David Protter <david.protter@gmail.com>
2023-04-25 16:35:50 -0600

    wow i have some work to do tonight

--- RPI_Operant/hardware/components.py
+++ RPI_Operant/hardware/components.py
@@ -177,20 +177,23 @@ class Lever:
         for fut in self.futures:
             if fut.done():
                 self.futures.remove(fut)
-    """ 
+
     @thread_it
     def _execute_move(self, wait = False):
         if not self.control_queue.empty():
+
             self.control_loc = True
             while self.control_loc and not self.box.finished():
                 
                 #get the destination and all incoming timestamp objects
                 destination, init_ts, finish_ts, interrupt_ts  = self.control_queue.get()
-                
+                print(f'lever {self.name} moving to destination {destination}')
                 if not self.angular_position:
-                    self.angular_position = self.retracted
+                    self.angular_position = abs(self.retracted - self.extended) / 2
                     
                 steps = int((self.angular_position - destination)/self.step_size)
+                
+                #where is the lever
                 loc = self.angular_position
                 interrupt = False
                 
@@ -205,14 +208,12 @@ class Lever:
                 init_ts.submit()
                 
                 for _ in range(steps):
-                    #step forward twice, back once, to help prevent binding
-                    loc+=step*2
-                    self.servo.angle = loc
-                    loc -= step
-                    
-                    
-                    self.servo.angle = loc
-                    
+                    #step slowly
+                    loc+=step
+                    try:
+                        self.servo.angle = loc
+                    except:
+                        print(f'{self.name} tried moving past allowable range. target:{loc}')
                     self.angular_position = loc
                     
                     #exit this loop if a new command is found
@@ -220,13 +221,14 @@ class Lever:
                         interrupt_ts.submit()
                         interrupt = True
                         break
-                    time.sleep(0.005)
+                    time.sleep(0.02)
                 
                 if not interrupt:
                     self.angular_position = destination
                     self.control_loc = False
                     
             finish_ts.submit()
+            self.disable()
             if destination == self.extended:
                 self.is_extended = True
             else:
@@ -245,12 +247,12 @@ class Lever:
         
         interrupt_ts = self.box.timestamp_manager.new_timestamp(description = oes.extend_interrupt+self.name, 
                                                         modifiers = {'ID':self.name})
-        self.control_queue.put((destination,start_ts, finish_ts, interrupt_ts))
-        if not self.control_loc:
-            self._execute_move(wait = wait)
+        self.control_queue.put((destination, start_ts, finish_ts, interrupt_ts))
+        self._execute_move(wait = wait)
             
         return self.box.timestamp_manager.new_latency(event_1 = oes.lever_extended+self.name, 
                                                         modifiers = {'ID':self.name})
+    
     def retract(self, wait = False):
         '''retract a lever and timestamp it
         returns a latency object that may be used to get the latency from lever-out to a second event'''
@@ -264,10 +266,10 @@ class Lever:
         interrupt_ts = self.box.timestamp_manager.new_timestamp(description = oes.retract_interrupt+self.name, 
                                                         modifiers = {'ID':self.name})
         self.control_queue.put((destination,start_ts, finish_ts, interrupt_ts))
-        if not self.control_loc:
-            self._execute_move(wait = wait) 
-            """
+        self._execute_move(wait = wait) 
+            
 
+    """     
     def extend(self, wait = False):
         '''extend a lever and timestamp it
         returns a latency object that may be used to get the latency from lever-out to a second event'''
@@ -299,15 +301,15 @@ class Lever:
         self.servo.angle = self.extended
         self.disable()
         self.is_extended = True
-        ts.submit()
+        ts.submit() """
 
     def disable(self):
         self.servo._pwm_out.duty_cycle = 0
         
-        
+    """     
     @thread_it
     def retract(self):
-        'extend a lever and timestamp it'
+        'retract a lever and timestamp it'
         #note, make a ts object and submit later after successful retraction
         ts = self.box.timestamp_manager.new_timestamp(description = oes.lever_retracted+self.name, modifiers = {'ID':self.name},
                                                       print_to_screen = False)
@@ -320,6 +322,9 @@ class Lever:
         
         numsteps = 20
         loc = self.servo.angle
+        if not loc:
+            print(f'servo {self.name} returned None for self.servo.angle')
+            loc = abs(self.extended - self.retracted) / 2
         step = (retract_start-loc)/numsteps
         
         self.box.timestamp_manager.new_timestamp(description = oes.start_lever_retract + self.name, modifiers = {'ID':self.name}, 
@@ -337,7 +342,7 @@ class Lever:
         self.disable()
         self.is_extended = False
         ts.submit()
-    
+     """
     @thread_it
     def watch_lever_pin(self):
         self.monitoring = True
